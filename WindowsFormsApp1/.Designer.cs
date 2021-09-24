6b21e1d5d61765c0b583be68a17fbfefba4e9d07
Elian-Jaramillo-Rojas <ejaramillo62@misena.edu.co>
2021-09-23 22:27:11 -0500

    se agrego codigo de un modulo

--- WindowsFormsApp1/.Designer.cs
+++ WindowsFormsApp1/.Designer.cs
@@ -31,16 +31,17 @@ namespace WindowsFormsApp1
         {
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.nombre = new System.Windows.Forms.TextBox();
+            this.opcion = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.apellidos = new System.Windows.Forms.TextBox();
+            this.correo = new System.Windows.Forms.TextBox();
+            this.contraseña = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.guardar = new System.Windows.Forms.Button();
+            this.fecha = new System.Windows.Forms.DateTimePicker();
             this.SuspendLayout();
             // 
             // label1
@@ -52,31 +53,25 @@ namespace WindowsFormsApp1
             this.label1.TabIndex = 0;
             this.label1.Text = "Nombre";
             // 
-            // textBox1
+            // nombre
             // 
-            this.textBox1.Location = new System.Drawing.Point(219, 34);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(166, 20);
-            this.textBox1.TabIndex = 1;
+            this.nombre.Location = new System.Drawing.Point(219, 34);
+            this.nombre.Name = "nombre";
+            this.nombre.Size = new System.Drawing.Size(166, 20);
+            this.nombre.TabIndex = 1;
+            this.nombre.TextChanged += new System.EventHandler(this.nombre_TextChanged);
             // 
-            // dateTimePicker1
+            // opcion
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(219, 198);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(355, 20);
-            this.dateTimePicker1.TabIndex = 2;
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
+            this.opcion.FormattingEnabled = true;
+            this.opcion.Items.AddRange(new object[] {
             "Profesor",
             "Estudiante",
             "Acudiente"});
-            this.comboBox1.Location = new System.Drawing.Point(302, 240);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(203, 21);
-            this.comboBox1.TabIndex = 3;
+            this.opcion.Location = new System.Drawing.Point(302, 240);
+            this.opcion.Name = "opcion";
+            this.opcion.Size = new System.Drawing.Size(203, 21);
+            this.opcion.TabIndex = 3;
             // 
             // label2
             // 
@@ -105,53 +100,74 @@ namespace WindowsFormsApp1
             this.label4.TabIndex = 6;
             this.label4.Text = "Apellidos";
             // 
-            // textBox2
+            // apellidos
             // 
-            this.textBox2.Location = new System.Drawing.Point(408, 34);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(166, 20);
-            this.textBox2.TabIndex = 7;
+            this.apellidos.Location = new System.Drawing.Point(408, 34);
+            this.apellidos.Name = "apellidos";
+            this.apellidos.Size = new System.Drawing.Size(166, 20);
+            this.apellidos.TabIndex = 7;
             // 
-            // textBox3
+            // correo
             // 
-            this.textBox3.Location = new System.Drawing.Point(219, 94);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(355, 20);
-            this.textBox3.TabIndex = 8;
+            this.correo.Location = new System.Drawing.Point(219, 94);
+            this.correo.Name = "correo";
+            this.correo.Size = new System.Drawing.Size(355, 20);
+            this.correo.TabIndex = 8;
             // 
-            // textBox4
+            // contraseña
             // 
-            this.textBox4.Location = new System.Drawing.Point(219, 156);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(355, 20);
-            this.textBox4.TabIndex = 9;
+            this.contraseña.Location = new System.Drawing.Point(219, 156);
+            this.contraseña.Name = "contraseña";
+            this.contraseña.Size = new System.Drawing.Size(355, 20);
+            this.contraseña.TabIndex = 9;
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.Aqua;
-            this.button1.Location = new System.Drawing.Point(357, 288);
+            this.button1.Location = new System.Drawing.Point(219, 298);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(91, 32);
             this.button1.TabIndex = 10;
-            this.button1.Text = "Guardar";
+            this.button1.Text = "Volver";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // guardar
+            // 
+            this.guardar.BackColor = System.Drawing.Color.Aqua;
+            this.guardar.Location = new System.Drawing.Point(483, 298);
+            this.guardar.Name = "guardar";
+            this.guardar.Size = new System.Drawing.Size(91, 32);
+            this.guardar.TabIndex = 11;
+            this.guardar.Text = "Guardar";
+            this.guardar.UseVisualStyleBackColor = false;
+            this.guardar.Click += new System.EventHandler(this.guardar_Click);
+            // 
+            // fecha
+            // 
+            this.fecha.CalendarMonthBackground = System.Drawing.SystemColors.InactiveBorder;
+            this.fecha.CalendarTitleForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.fecha.Location = new System.Drawing.Point(219, 203);
+            this.fecha.Name = "fecha";
+            this.fecha.Size = new System.Drawing.Size(355, 20);
+            this.fecha.TabIndex = 26;
             // 
             // Crear_Cuenta
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.fecha);
+            this.Controls.Add(this.guardar);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.contraseña);
+            this.Controls.Add(this.correo);
+            this.Controls.Add(this.apellidos);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.dateTimePicker1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.opcion);
+            this.Controls.Add(this.nombre);
             this.Controls.Add(this.label1);
             this.Name = "Crear_Cuenta";
             this.ResumeLayout(false);
@@ -163,15 +179,16 @@ namespace WindowsFormsApp1
 
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox nombre;
+        private System.Windows.Forms.ComboBox opcion;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox apellidos;
+        private System.Windows.Forms.TextBox correo;
+        private System.Windows.Forms.TextBox contraseña;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button guardar;
+        private System.Windows.Forms.DateTimePicker fecha;
     }
 }
---
+++ WindowsFormsApp1/Cglobal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Cglobal
+    {
+        public static List<registrar> listaValores = new List<registrar>();
+    }
+}
